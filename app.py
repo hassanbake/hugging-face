3463a39ae63fb2c8399de3837e8005d659a3087c
Maherolnaghsh Rouzbeh <R.Maherolnaghsh@etadbir.com>
2024-01-13 14:44:37 +0330

    adding the first transformer

--- app.py
+++ app.py
@@ -1,18 +1,9 @@
-from transformer import Classification, Generator
+from transformers import pipeline
 
 def main():
-    classification = Classification()
-
-    result = classification.classify(["I have been waiting for Huugging face course my whole life", 
-                                   "I hate this so much."])
-    print(result)
-
-    result = classification.classify("This is a course about Transformer libarary.",
-                                  ["education","politics","business"])
-    print(result)
-
-    result = Generator.generator("In this course, we will teach you how to")
-    print(result)
+    classifier = pipeline("sentiment-analysis")
+    res = classifier("I have been waiting for Hugging Face course my whole life.")
+    print(res)
 
 if __name__ == "__main__":
     main()
